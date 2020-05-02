3c180e5344b9f717e157c7ea4adab99d83a1304f
vividswan <vividswan@naver.com>
2020-05-02 23:41:14 +0900

    Create SecondServlet.java

---
+++ study/src/com/study/test/SecondServlet.java
@@ -0,0 +1,18 @@
+package com.study.test;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/second")
+public class SecondServlet extends HttpServlet{
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("SecondServlet!!");
+	}
+
+}
