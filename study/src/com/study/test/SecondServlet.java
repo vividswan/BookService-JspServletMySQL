547d2c31c2b404175386507b0b23cec24b8f2dc1
vividswan <vividswan@naver.com>
2020-05-03 14:07:31 +0900

    Update SecondServlet.java

--- study/src/com/study/test/SecondServlet.java
+++ study/src/com/study/test/SecondServlet.java
@@ -1,6 +1,7 @@
 package com.study.test;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -13,6 +14,12 @@ public class SecondServlet extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("SecondServlet!!");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<title><head>Test</head></title>");
+		out.println("<body><h1>Hello World!</h1></body>");
+		out.println("</hmtl>");
+		out.close();
 	}
 
 }
