270788039a344a0eff315963ef2bbdd3cadc4cc8
vividswan <vividswan@naver.com>
2020-05-06 23:31:19 +0900

    Create ThirdServlet.java

---
+++ study/src/com/study/test/ThirdServlet.java
@@ -0,0 +1,21 @@
+package com.study.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/third")
+public class ThirdServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>좋은 하루!</h1>");
+		out.close();
+	}
+}
