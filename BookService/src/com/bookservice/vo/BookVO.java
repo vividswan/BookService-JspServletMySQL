25da839d2502a260f0b290a08824cf494b0ec5da
vividswan <vividswan@naver.com>
2020-06-06 23:55:23 +0900

    Create BookVO.java

---
+++ BookService/src/com/bookservice/vo/BookVO.java
@@ -0,0 +1,47 @@
+package com.bookservice.vo;
+
+public class BookVO {
+	private int ID;
+	private String name;
+	private String author;
+	private int price;
+	private boolean isBorrow;
+	private int borrowCnt;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public boolean isBorrow() {
+		return isBorrow;
+	}
+	public void setBorrow(boolean isBorrow) {
+		this.isBorrow = isBorrow;
+	}
+	public int getBorrowCnt() {
+		return borrowCnt;
+	}
+	public void setBorrowCnt(int borrowCnt) {
+		this.borrowCnt = borrowCnt;
+	}	
+}
