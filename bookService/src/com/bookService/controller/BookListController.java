8d44f6053957c17e7808a4ce747a157e109cc608
vividswan <vividswan@naver.com>
2020-06-09 03:22:41 +0900

    Update BookListController.java

--- bookService/src/com/bookService/controller/BookListController.java
+++ bookService/src/com/bookService/controller/BookListController.java
@@ -7,18 +7,27 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.bookService.bean.BookBean;
 import com.bookService.service.BookService;
+import com.bookService.vo.BookVO;
 
 public class BookListController implements Controller {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		BookService service = BookService.getInstance();
-		ArrayList<BookBean> list = service.bookList();
-		
+		ArrayList<BookVO> list = service.bookList();
 		request.setAttribute("list", list);
-		HttpUtil.forward(request, response, "/result/bookListOutput.jsp");
+		
+		String indexFlag = (String)request.getAttribute("indexFlag");
+		if(indexFlag != null &&indexFlag.equals("firstPage")) {
+			HttpUtil.forward(request, response, "/result/bookListOutputFirst.jsp");
+		}else if(indexFlag != null &&indexFlag.equals("sortPage")) {
+			HttpUtil.forward(request, response, "/result/bookListOutputSort.jsp");
+		}else if(indexFlag != null &&indexFlag.equals("borrowedBookPage")) {
+			HttpUtil.forward(request, response, "/result/bookListOutputBorrow.jsp");
+		}else if(indexFlag != null &&indexFlag.equals("notBorrowedBookPage")) {
+			HttpUtil.forward(request, response, "/result/bookListOutputNotBorrow.jsp");
+		}else HttpUtil.forward(request, response, "/result/bookListOutput.jsp");
 	}
 	
 }
