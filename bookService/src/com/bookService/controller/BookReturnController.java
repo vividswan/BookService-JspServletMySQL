cd8b35e379de8cc67d38edcfa4b98e60c3632baf
vividswan <vividswan@naver.com>
2020-06-09 03:22:44 +0900

    Create BookReturnController.java

---
+++ bookService/src/com/bookService/controller/BookReturnController.java
@@ -0,0 +1,22 @@
+package com.bookService.controller;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+import com.bookService.service.BookService;
+
+
+public class BookReturnController implements Controller {
+
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String _ID = (String)request.getAttribute("ID");
+		int ID = Integer.parseInt(_ID);
+		
+		BookService service = BookService.getInstance();
+		service.bookReturn(ID);
+		HttpUtil.forward(request, response, "bookList.action");
+	}
+
+
+}
