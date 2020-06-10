f6119d57488538bd993fccfd0b1dc01226baf79c
vividswan <vividswan@naver.com>
2020-06-10 13:55:06 +0900

    Create BookDeleteListController.java

---
+++ bookService/src/com/bookService/controller/BookDeleteListController.java
@@ -0,0 +1,24 @@
+package com.bookService.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bookService.service.BookService;
+import com.bookService.vo.BookVO;
+
+public class BookDeleteListController implements Controller {
+
+	@Override
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BookService service = BookService.getInstance();
+		ArrayList<BookVO> list = service.bookList();
+		
+		request.setAttribute("list", list);
+		HttpUtil.forward(request, response, "/result/bookDeleteListOutput.jsp");
+	}
+	
+}
