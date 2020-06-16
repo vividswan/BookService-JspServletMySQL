49018f55c73333a55d9ea480d4bd18b65a88a2de
vividswan <vividswan@naver.com>
2020-06-16 22:01:26 +0900

    Update BookListController.java

--- bookService/src/com/bookService/controller/BookListController.java
+++ bookService/src/com/bookService/controller/BookListController.java
@@ -14,11 +14,33 @@ public class BookListController implements Controller {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		BookService service = BookService.getInstance();
-		ArrayList<BookVO> list = service.bookList();
-		request.setAttribute("list", list);
+		String strUpperSeqNo = request.getParameter("LAST_SEQ_NO");
+		int upperSeqNo = 0;
+		if (strUpperSeqNo == null)
+			upperSeqNo = 0;
+		else
+			upperSeqNo = Integer.parseInt(strUpperSeqNo);
 		
 		String indexFlag = (String)request.getAttribute("indexFlag");
+		
+		BookService service = BookService.getInstance();
+		if(indexFlag != null &&indexFlag.equals("sortPage")) {
+			ArrayList<BookVO> list = service.bookListByBorrowCnt(upperSeqNo);
+			request.setAttribute("list", list);
+		}else if (indexFlag != null &&indexFlag.equals("notBorrowedBookPage")) {
+			ArrayList<BookVO> list = service.bookListNotBorrowed(upperSeqNo);
+			request.setAttribute("list", list);
+		}else if (indexFlag != null &&indexFlag.equals("borrowedBookPage")) {
+			ArrayList<BookVO> list = service.bookListBorrowed(upperSeqNo);
+			request.setAttribute("list", list);
+		}else if (indexFlag != null &&indexFlag.equals("bookReturn")) {
+			ArrayList<BookVO> list = service.bookListBorrowed(upperSeqNo);
+			request.setAttribute("list", list);
+		}else {
+		ArrayList<BookVO> list = service.bookList(upperSeqNo);
+		request.setAttribute("list", list);
+		}
+		
 		if(indexFlag != null &&indexFlag.equals("firstPage")) {
 			HttpUtil.forward(request, response, "/result/bookListOutputFirst.jsp");
 		}else if(indexFlag != null &&indexFlag.equals("sortPage")) {
@@ -27,7 +49,11 @@ public class BookListController implements Controller {
 			HttpUtil.forward(request, response, "/result/bookListOutputBorrow.jsp");
 		}else if(indexFlag != null &&indexFlag.equals("notBorrowedBookPage")) {
 			HttpUtil.forward(request, response, "/result/bookListOutputNotBorrow.jsp");
-		}else HttpUtil.forward(request, response, "/result/bookListOutput.jsp");
+		}else if(indexFlag != null &&indexFlag.equals("bookReturn")) {
+			HttpUtil.forward(request, response, "/result/bookReturnListOutput.jsp");
+		}else if(indexFlag != null && indexFlag.equals("bookDeletePage")) {
+			HttpUtil.forward(request, response, "/result/bookDeleteListOutput.jsp");
+		}else HttpUtil.forward(request, response, "/result/bookListOutput.jsp?upperSeqNo="+upperSeqNo);
 	}
 	
 }
